a2c3e3eebf5c85946a9a279cb0aa94a81b59653f
Eric Sun <suneric98@gmail.com>
2020-10-09 00:17:05 -0400

    added grouping

--- Emojifier/parsing/generator.py
+++ Emojifier/parsing/generator.py
@@ -5,24 +5,47 @@ import os
 import re
 
 EMOJI_MAPPING_FILE = os.path.join('Emojifier','data','emoji-mapping2.json')
+MAX_EMOJIS = 2
+
+def makeGroups(lst, n):
+    groups = []
+    for i in range(0, len(lst), n):
+        groups.append(lst[i:i+n])
+    return groups
 
 def getEmojiMapping():
     mappings = None
     with io.open(EMOJI_MAPPING_FILE, "r", encoding="utf-8") as mappings_file:
         mappings = json.load(mappings_file)
     return mappings
-    
-def generateEmojipasta(text, mapping):
-    tokens = text.split()
+
+def getEmojisForGroups(groups, mapping):
     result = []
-    for t in tokens:
-        result.append(t)
-        t = re.sub(r'[^\w\s]', '', t.lower())
-        if t in mapping:
-            if "before" in mapping[t]:
-                result.append(random.choice(mapping[t]["before"]))
-            if "after" in mapping[t]:
-                result.append(random.choice(mapping[t]["after"]))
+    for g in groups:
+        beforeEmojis = []
+        afterEmojis = []
+        for t in g:
+            t = re.sub(r'[^\w\s]', '', t.lower())
+            if t in mapping:
+                if "before" in mapping[t]:
+                    beforeEmojis.extend(mapping[t]["before"])
+                if "after" in mapping[t]:
+                    afterEmojis.extend(mapping[t]["after"])
+        
+        numBefore = int(MAX_EMOJIS/2) if beforeEmojis else 0
+        numAfter = int(MAX_EMOJIS/2) if afterEmojis else 0
+        for i in range(0, numBefore):
+            result.append(random.choice(beforeEmojis))
+        result.extend(g)
+        for i in range(0, numAfter):
+            result.append(random.choice(afterEmojis))
+    return result
+        
+
+def generateEmojipasta(text, mapping, freq = 2):
+    tokens = text.split()
+    groups = makeGroups(tokens, freq)
+    result = getEmojisForGroups(groups, mapping)
     return " ".join(result)
 
 def main():

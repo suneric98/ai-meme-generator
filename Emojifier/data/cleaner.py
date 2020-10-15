1b4014f9f279949c894d33dd06326bba3ec1ed89
Marvin <myy22@cornell.edu>
2020-10-15 17:44:46 -0400

    Cleaned twitter-data.txt
    - Removed all @words, hashtags, and links

---
+++ Emojifier/data/cleaner.py
@@ -0,0 +1,30 @@
+# load text
+f = open("twitter-data.txt", "r", encoding="utf-8")
+text = f.read()
+f.close()
+
+# split into individual tweets
+lines = text.splitlines()
+
+# remove all @words, #'s, and links
+for i in range(len(lines)):
+    l = lines[i]
+    if (l.find("@") != -1) or (l.find("#") != -1) or (l.find("https")):
+        words = l.split()
+        for j in range(len(words)):
+            w = words[j]
+            if w.find("@") != -1:
+                words[j] = ""
+            elif w.find("#") != -1:
+                words[j] = w.split("#")[1]
+            elif w.find("https") != -1:
+                words[j] = ""
+        cleaned = " ".join(words)
+        lines[i] = cleaned.strip()
+
+# write out to a new file
+out = open("twitter-data-cleaned.txt", "w", encoding="utf-8")
+for l in lines:
+    out.write(l)
+    out.write("\n")
+out.close()

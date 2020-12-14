4a4b80a24f5f2691c8b22716584021b5ba42fe7b
Marvin <myy22@cornell.edu>
2020-12-14 16:37:54 -0500

    visualization for meme generator complete

--- web-app/app/meme/tfidf_generator.py
+++ web-app/app/meme/tfidf_generator.py
@@ -5,33 +5,49 @@ from sklearn.linear_model import SGDClassifier
 import json
 import numpy as np
 import random
+import pandas as pd
 
-vectorizer = load('meme_vectorizer.joblib')
-clf = load('meme_predictor.joblib')
+vectorizer = load('./app/meme/meme_vectorizer.joblib')
+clf = load('./app/meme/meme_predictor.joblib')
 selected_memes = [
-    'Woman-Yelling-At-Cat.json', 
-    'Left-Exit-12-Off-Ramp.json', 
-    'Surprised-Pikachu.json',    
-    'Is-This-A-Pigeon.json',  
-    'Drake-Hotline-Bling.json', 
-    'Blank-Nut-Button.json', 
-    'One-Does-Not-Simply.json',
-    'Change-My-Mind.json', 
-    'Roll-Safe-Think-About-It.json', 
-    'Leonardo-Dicaprio-Cheers.json' 
+    'Woman Yelling At Cat',
+    'Left Exit 12 Off Ramp',
+    'Surprised Pikachu',
+    'Is This A Pigeon',
+    'Drake Hotline Bling',
+    'Blank Nut Button',
+    'One Does Not Simply',
+    'Change My Mind',
+    'Roll Safe Think About It',
+    'Leonardo Dicaprio Cheers'
 ]
 
 # Input: text to predict meme format
 # Returns: meme prediction (in text form)
+
+
 def generateMemePrediction(text):
     text = text.strip().lower()
     feats = vectorizer.transform([text])
     pred = clf.predict(feats)[0]
+    generateInsight(text)
+    generateProbability(text)
     return selected_memes[pred]
 
 # Input: text to predict meme format
 # Returns: probability for each meme class
+
+
 def generateProbability(text):
+    text = text.strip().lower()
+    feats = vectorizer.transform([text])
+    pred = clf.predict_proba(feats)[0]
+
+    df = pd.DataFrame(pred)
+    df.to_csv('./app/csv/meme_probability.csv', index=False, header=True)
+
+
+def generateProbability2(text):
     text = text.strip().lower()
     feats = vectorizer.transform([text])
     pred = clf.predict_proba(feats)[0]
@@ -40,16 +56,28 @@ def generateProbability(text):
 # Input: text to predict meme format
 # Returns: a list for each word that shows the difference between the OG
 # probabilities and the probability of missing that word
+
+
 def generateInsight(text):
     text = text.strip().lower()
     words = text.split()
-    default = generateProbability(text)
+    default = generateProbability2(text)
     probForWords = []
-    for i,_ in enumerate(words):
-        probs = generateProbability(' '.join(words[:i] + words[i+1:]))
+    for i, _ in enumerate(words):
+        probs = generateProbability2(' '.join(words[:i] + words[i+1:]))
         p = np.array(default) - np.array(probs)
         probForWords.append(p.tolist())
-    return probForWords
+
+    for i in range(len(probForWords)):
+        probForWords2 = [abs(x) for x in probForWords[i]]
+        maximum = max(probForWords2)
+        if maximum > 0:
+            probForWords[i] = [round(x/maximum, 4) for x in probForWords2]
+
+    df = pd.DataFrame(probForWords)
+    df.insert(0, -1, words)
+    df.to_csv('./app/csv/meme_insights.csv', index=False, header=True)
+
 
 def main():
     while True:
@@ -57,9 +85,10 @@ def main():
         text = input()
         if text == "quit":
             break
-        answer = generateMemePrediction(text)
+        answer = generateProbability2(text)
         print(answer)
-        answer = generateInsight(text)
+        #answer = generateInsight(text)
+
 
 if __name__ == "__main__":
     main()
--- web-app/app/routes.py
+++ web-app/app/routes.py
@@ -3,6 +3,7 @@ from app import app
 from app.forms import EmojiForm
 from app.forms import MemeForm
 from .emoji.tfidf_generator import generateEmojipasta
+from .meme.tfidf_generator import generateMemePrediction
 
 
 @app.route('/')
@@ -24,8 +25,8 @@ def emoji():
 def meme():
     form = MemeForm()
     if form.validate_on_submit():
-        flash('Output: {}'.format(form.input_s.data))
-        return redirect(url_for('meme'))
+        flash('Output: {}'.format(generateMemePrediction(form.input_s.data)))
+        return redirect(url_for('meme_vis'))
     return render_template('meme.html', title='Meme Classifier', form=form)
 
 
@@ -34,7 +35,24 @@ def emoji_vis():
     return render_template('emoji_vis.html', title="Emoji Visualization")
 
 
+@app.route('/meme_vis', methods=['GET', 'POST'])
+def meme_vis():
+    return render_template('meme_vis.html', title="Meme Visualization")
+
+
 @app.route('/csv/emoji_insights.csv', methods=['GET'])
 def emoji_insight():
     with open("./app/csv/emoji_insights.csv", "r") as fp:
+        return fp.read()
+
+
+@app.route('/csv/meme_insights.csv', methods=['GET'])
+def meme_insight():
+    with open("./app/csv/meme_insights.csv", "r") as fp:
+        return fp.read()
+
+
+@app.route('/csv/meme_probability.csv', methods=['GET'])
+def meme_probability():
+    with open("./app/csv/meme_probability.csv", "r") as fp:
         return fp.read()
